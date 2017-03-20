d3d674b6901087c6c9872ea947b7793ca4bdddda
berglindoma13 <berglindoma13@ru.is>
2017-03-20 22:32:25 +0000

    added ugly pipe and fixed constant movement of bottom

--- app/scripts/world.js
+++ app/scripts/world.js
@@ -5,24 +5,44 @@
      'use strict';
 
      var SPEED = 30;
+     var WIDTH = 20;
 
-     var World = function(down, game){
+     var World = function(pipe1, down, game){
          this.game = game;
-
+         this.pipe1 = pipe1;
          this.down = down;
-       this.down.pos = {x: 0, y: 0};
+
+         this.pipe1.pos = {x: 0, y:0};
+         this.down.pos = {x: 0, y: 0};
      };
 
      World.prototype.reset = function(){
          this.down.pos.x = 0;
+         this.pipe1.pos.x = 120;
 
+         //Change this to a random number
+         this.pipe1.pos.y = 30;
+
+         this.pipe1.css('transform', 'translateZ(0) translate(' + this.pipe1.pos.x + 'em, ' + this.pipe1.pos.y + 'em)');
          this.down.css('transform', 'translateZ(0) translate(' + this.down.pos.x + 'em, ' + this.down.pos.y + 'em)');
      };
 
      World.prototype.onFrame = function(delta){
 
+         this.outOfBounds();
+
          this.down.pos.x -= (delta * SPEED);
+         this.pipe1.pos.x -= (delta * SPEED);
+
+         this.pipe1.css('transform', 'translateZ(0) translate(' + this.pipe1.pos.x + 'em, ' + this.pipe1.pos.y + 'em)');
          this.down.css('transform', 'translateZ(0) translate(' + this.down.pos.x + 'em, ' + this.down.pos.y + 'em)');
+     };
+
+     World.prototype.outOfBounds = function(){
+         //THIS RESETS THE SCENE TO LOOK LIKE ITS CONSTANTLY MOVING
+         if(this.pipe1.pos.x < -WIDTH){
+             return this.reset();
+         }
      };
 
      return World;
